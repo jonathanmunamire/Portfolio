0750b7d4b0f0b86ae8c696abfab46b92e8b30041
jonathanmunamire <jonathanmunamire@gmail.com>
2022-10-11 14:48:06 +0200

    Add Menu Effects

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ hamburger.addEventListener('click',function() {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
 });
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    }))
