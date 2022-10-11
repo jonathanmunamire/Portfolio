922a27380f359aa21aca29783760e9ace4a8299a
Ermi <manermidem@gmail.com>
2022-10-11 13:38:32 +0300

    Edit the js file for the nav menu

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/*let hamburger = document.querySelector(".hamburger");
-let nav = document.querySelector(".nav");
-
-hamburger.addEventListener("Click", function() {
-    alert('It was clicked!');
-})*/
-
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
